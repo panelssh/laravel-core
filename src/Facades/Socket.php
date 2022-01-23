d1bca240002fd108348f91878a7b74267fb7358c
panelssh <panelssh@hotmail.com>
2022-01-23 16:30:15 +0700

    fix(facades): added static method on docblock

--- src/Facades/Socket.php
+++ src/Facades/Socket.php
@@ -5,6 +5,12 @@ namespace PanelSsh\Core\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \PanelSsh\Core\Libraries\Socket setHost(string $host)
+ * @method static \PanelSsh\Core\Libraries\Socket setPort(int $port)
+ * @method static \PanelSsh\Core\Libraries\Socket setSecretKey(string $secretKey)
+ * @method static \PanelSsh\Core\Libraries\Socket setCommand(string $command)
+ * @method static \PanelSsh\Core\Libraries\Socket setPayload(array $host)
+ * @method static object connect()
  * @see \PanelSsh\Core\Libraries\Socket
  */
 class Socket extends Facade
--- src/Facades/Tunnel.php
+++ src/Facades/Tunnel.php
@@ -2,9 +2,17 @@
 
 namespace PanelSsh\Core\Facades;
 
+use Closure;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \PanelSsh\Core\Libraries\Tunnel setHost(string $host)
+ * @method static \PanelSsh\Core\Libraries\Tunnel setPort(int $port)
+ * @method static \PanelSsh\Core\Libraries\Tunnel setUsername(string $username)
+ * @method static \PanelSsh\Core\Libraries\Tunnel setPassword(string $password)
+ * @method static \PanelSsh\Core\Libraries\Tunnel setCommand(string $command)
+ * @method static \PanelSsh\Core\Libraries\Tunnel setPayload(array $host)
+ * @method static \PanelSsh\Core\Libraries\Tunnel connect(Closure $callback = null)
  * @see \PanelSsh\Core\Libraries\Tunnel
  */
 class Tunnel extends Facade
